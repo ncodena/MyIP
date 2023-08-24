a4e5f35a3193f23e8ab8a9b199570889d8eb0674
Nuria <c.odena.nuria@gmail.com>
2023-08-24 20:13:50 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios';
+import Map from './components/Map';
+
+function App() {
+  const [ipAddress, setIPAddress] = useState('');
+  const [data, setData] = useState({});
+  const [countryInfo, setCountryInfo] = useState({});
+
+
+  useEffect(() => {
+    const fetchIPAddress = async () => {
+      try {
+        const apiKey = import.meta.env.VITE_REACT_APP_IPIFY_API_KEY;
+        const response = await axios.get(`https://geo.ipify.org/api/v1?apiKey=${apiKey}`);
+        const data = response.data;
+        setIPAddress(data.ip);
+        setData(data.location)
+        try {
+          const response2 = await axios.get(`https://restcountries.com/v3.1/alpha/${data.location.country}`);
+          const country = response2.data;
+          setCountryInfo(country[0]);
+        } catch (error) {
+          console.error('Error fetching user country:', error);
+        }
+      } catch (error) {
+        console.error('Error fetching IP address:', error);
+      }
+    };
+
+    fetchIPAddress();
+  }, []);
+
+  return (
+    <>
+      <h2>Your IP Address:</h2>
+      <p>{ipAddress}</p>
+      {data ? <Map data={data}  /> : null }
+      { Object.keys(data).length > 0 ? (
+        <>
+          <h2>{countryInfo.name.common}</h2>
+          <img src={countryInfo.flags.png} alt={countryInfo.flags.alt} />
+        </>
+      ) : null}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Map.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import axios from 'axios';
+
+const Map = ({data}) => {
+  return (
+    <div className="map-container">
+      { Object.keys(data).length > 0 ? (
+        <MapContainer center={[data.lat, data.lng]}  zoom={13} scrollWheelZoom={false} style={{ height: '500px' }}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={[data.lat, data.lng]}>
+            <img src={`https://www.countryflags.io/${data.country}/flat/64.png`} alt="Flag" />
+          </Marker>
+        </MapContainer>
+      ) : null}
+    </div>
+  );
+};
+
+export default Map;
